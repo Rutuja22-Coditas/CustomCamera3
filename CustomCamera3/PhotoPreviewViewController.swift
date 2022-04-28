ce42bc0b8064ffde915e0b646f50a182f241b401
Rutuja Hulsure <rutuja.hulsure@coditas.com>
2022-04-28 10:25:38 +0530

    PhotoPreviewViewController to show selected image.

---
+++ CustomCamera3/PhotoPreviewViewController.swift
@@ -0,0 +1,15 @@
+
+
+import UIKit
+
+class PhotoPreviewViewController: UIViewController {
+
+    @IBOutlet weak var selectedImageView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
